4e78ddb8ad3419d399f21c49359960e1b7e3b0a4
Tran Trung <baotrungtn@Trans-MacBook-Pro.local>
2017-05-05 12:06:35 +0900

    TextView can do Next

--- app/src/main/java/com/trungbao/geoquiz/QuizActivity.java
+++ app/src/main/java/com/trungbao/geoquiz/QuizActivity.java
@@ -79,5 +79,14 @@ public class QuizActivity extends AppCompatActivity {
                 updateQuestion();
             }
         });
+
+        mQuestionTextView.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                updateQuestion();
+            }
+        });
     }
 }

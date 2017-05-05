1151e1ab2ad6f8604b11a41841ca99798df5b7e3
Tran Trung <baotrungtn@Trans-MacBook-Pro.local>
2017-05-05 12:23:45 +0900

    Add Prev Button

--- app/src/main/java/com/trungbao/geoquiz/QuizActivity.java
+++ app/src/main/java/com/trungbao/geoquiz/QuizActivity.java
@@ -13,6 +13,8 @@ public class QuizActivity extends AppCompatActivity {
     private Button mTrueButton;
     private Button mFalseButton;
     private Button mNextButton;
+    private Button mPrevButton;
+
     private TextView mQuestionTextView;
 
     private Question[] mQuestionBank = new Question[] {
@@ -76,6 +78,16 @@ public class QuizActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                updateQuestion();
+            }
+        });
+
+        mPrevButton = (Button) findViewById(R.id.prev_button);
+        mPrevButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                mCurrentIndex = (mCurrentIndex - 1 + mQuestionBank.length) % mQuestionBank.length;
                 updateQuestion();
             }
         });
